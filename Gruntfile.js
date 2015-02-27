47ef8485b0eb574420ce7dc8f04b97ebe504c8dd
Tim Shultis <tim@morsekode.com>
2015-02-26 23:24:55 -0600

    grunt livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,14 @@
 'use strict';
 
-
+//enable modrewite (httaccess like) and mount the app directory
+var modRewrite = require('connect-modrewrite');
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
 
 module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-connect');
-
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.initConfig({
         // Project settings
@@ -12,12 +16,24 @@ module.exports = function(grunt) {
             // Configurable paths
             app: 'app',
         },
-        connect: {
-            server: {
+        // Watches files for changes and runs tasks based on the changed files
+        watch: {
+            livereload: {
                 options: {
-                    port: 8000,
-                    hostname: 'localhost'
-                }
+                    livereload: '<%= connect.options.livereload %>'
+                },
+                files: [
+                    '<%= config.app %>/{,*/}*.html',
+                    '<%= config.app %>/images/{,*/}*',
+                    '<%= config.app %>/scripts/*.js'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 8000,
+                livereload: 35729,
+                hostname: 'localhost'
             },
             dev: {
                 options: {
@@ -25,12 +41,29 @@ module.exports = function(grunt) {
                         '<%= config.app %>'
                     ]
                 }
-            }
+            },
+            livereload: {
+                options: {
+                    open: true,
+                    base: [
+                        '<%= config.app %>'
+                    ],
+                    //htaccess like middleware
+                    //currently set to route all traffic through index.html
+                    middleware: function (connect) {
+                        return [
+                            modRewrite (['!\\.html|\\.scss|\\.js|\\.svg|\\.css|\\.png|\\.jpg$ /index.html']),
+                            mountFolder(connect, 'app'),
+                        ];
+                    }
+                }
+            },
         }
 
     });
     grunt.registerTask('serve', [
-        'connect:dev:keepalive'
+        'connect:livereload',
+        'watch'
     ]);
 
 };
